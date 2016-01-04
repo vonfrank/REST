2d2ddbe6ac937ec9bbd04ffcdadbfdf12e446a34
von Frank <henrik@vonfrank.dk>
2016-01-04 15:01:59 +0100

    Final commit

---
+++ Client/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    [DataContract]
+    class Post
+    {
+        [DataMember]
+        int userId { get; set; }
+        [DataMember]
+        int id { get; set; }
+        [DataMember]
+        string title { get; set; }
+        [DataMember]
+        string body { get; set; }
+
+        public string ToString()
+        {
+            return "User ID: " + userId.ToString() + "\nID: " + id.ToString() + "\nTitle: " + title + "\nBody: " + body;
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,29 @@ namespace Client
 {
     class Program
     {
+        public static Post GetData()
+        {
+            Post jsonPost;
+            var url = "http://jsonplaceholder.typicode.com/posts/1";
+
+            //Syncronious consumption
+            var syncClient = new WebClient();
+            var content = syncClient.DownloadString(url);
+
+            //Create JSON serializer and parse the response
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Post));
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(content)))
+            {
+                jsonPost = (Post)serializer.ReadObject(ms);
+            }
+            return jsonPost;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Fetching JSON from http://jsonplaceholder.typicode.com/posts/1...\n");
+            Console.WriteLine(GetData().ToString());
+            Console.ReadLine();
         }
     }
 }
